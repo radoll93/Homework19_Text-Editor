dec323078e8a0500b5135ef3160f2892fa793179
Bora Lee <bbora1119@gmail.com>
2022-01-17 11:39:48 -0500

    copy the intial file structure and fill the webpack, and client side js files

---
+++ client/src/js/database.js
@@ -0,0 +1,54 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    const contactDb = await openDB('jate', 1);
+
+    const tx = contactDb.transaction('jate', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const result = await store.add({ content: content });
+
+    console.log('data saved to the database', result)
+
+  } catch {
+    
+    console.error('putDb not implemented');
+  }
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try{
+    const contactDb = await openDB('jate', 1);
+    const tx = contactDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+
+
+  } catch {
+    
+    console.error('getDb not implemented');
+
+  }
+}
+
+initdb();
